9684b2b5d36e71af96d458b25f98c8a6e555d1f1
awaisajmal12 <76112390+awaisajmal12@users.noreply.github.com>
2020-12-19 17:31:16 +0000

    'Class-4'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+// eslint-disable-next-line
+import react, {useState} from 'react';
 import './App.css';
+import {Button} from './Button.js';
 
 function App() {
+  let [count, setCount] = useState(0)
+  let [islight, setlight] = useState(true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= {`box ${islight ? 'light' : 'red'}`}>
+        <h1> light = {islight ? 'On' : 'Off'}</h1>
+     <Button counter={count}/>
+      <br/>
+      <button onClick={()=> setCount(++count) }>
+        update counter </button>
+        <br/>
+        <button onClick= {()=> setlight(!islight) }>
+          update light 
+        </button>
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line
+import react from 'react';
+
+
+export function Button(props) {
+
+    return (
+
+        <h1>
+            {props.counter}
+        </h1>   
+    )
+}
